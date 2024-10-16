ca9911db181c4e5f068bf8be698cfcc3fbb79c10
yashi <yashi@qq.com>
2024-10-17 01:07:27 +0800

    3

--- ConsoleAppForCSharpMath/Program.cs
+++ ConsoleAppForCSharpMath/Program.cs
@@ -24,6 +24,21 @@
             //Console.WriteLine(Math.Max(2.2, 3.8));
 
             //Console.WriteLine(Math.Min(2.2, 3.8));
+
+            //Console.WriteLine(Math.Tan(2.3));
+
+            //Console.WriteLine(Math.Sin(2.3));
+
+            //Console.WriteLine(Math.Pow(2, 3));
+
+            //Console.WriteLine(Math.Log2(2));
+
+            //Console.WriteLine(Math.Log10(1));
+
+            //Console.WriteLine(Math.Log(2, 2));
+            //Console.WriteLine(Math.Log(2));
+
+            //Console.WriteLine(Math.Cos(2.3));
         }
     }
 }
